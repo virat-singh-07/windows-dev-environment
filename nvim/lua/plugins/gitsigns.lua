f2c421a5d1a7f66e3e1f170198a5c2479fac3790
Virat Singh <virat.singh@wescodist.com>
2026-07-19 01:15:12 +0530

    Initial setup for nvim part 4

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -1,8 +1,35 @@
 return {
 	"lewis6991/gitsigns.nvim",
 	opts = {
-		onAttach = function()
-			-- Here will add the requried things 
+		on_attach = function(bufnr)
+			local gs = package.loaded.gitsigns
+
+			vim.keymap.set("n", "]h", function()
+				gs.next_hunk()
+			end, {
+				buffer = bufnr,
+			})
+			vim.keymap.set("n", "[h", function()
+				gs.prev_hunk()
+			end, {
+				buffer = bufnr,
+			})
+			vim.keymap.set("n", "<leader>hp", function()
+				gs.preview_hunk()
+			end, {
+				buffer = bufnr,
+			})
+			vim.keymap.set("n", "<leader>hs", function()
+				gs.stage_hunk()
+			end, {
+				buffer = bufnr,
+			})
+			vim.keymap.set("n", "<leader>hu", function()
+				gs.undo_stage_hunk()
+			end, {
+				buffer = bufnr,
+			})
 		end,
 	},
 }
+-- This is the check for the same for the git sign
